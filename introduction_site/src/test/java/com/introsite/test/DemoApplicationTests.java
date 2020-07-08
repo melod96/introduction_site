6e781519af9eb7a47750d0af0912359ddda76cbd
hw.lee <hyeongu0305@gmail.com>
2020-07-08 18:31:46 +0900

    초기 세팅 관련 내용

---
+++ introduction_site/src/test/java/com/introsite/test/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.introsite.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
