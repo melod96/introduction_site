3397d3884ec60532908bce2e3e9a16aa54b40a0e
hw.lee <hyeongu0305@gmail.com>
2020-07-10 18:33:01 +0900

    logback 관련 내용 추가

--- introduction_site/src/main/java/com/introsite/main/controller/MainController.java
+++ introduction_site/src/main/java/com/introsite/main/controller/MainController.java
@@ -2,6 +2,8 @@ package com.introsite.main.controller;
 
 import com.introsite.main.service.MainService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +12,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/main")
 public class MainController {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	public MainService mainService;
 
 	@RequestMapping("/go")
 	public String goMain() {
-		System.out.println("############MainController.goMain : 정상 진입 성공");
-		System.out.println("############MainController.goMain : 데이터 조회");
+
+
+		logger.trace("TRACE Level 테스트");
+		logger.info("INFO Level 테스트");
+		logger.debug("DEBUG Level 테스트");
+		logger.warn("WARN Level 테스트");
+		logger.error("ERROR Level 테스트");
+
+		logger.debug("MainController.goMain : 정상 진입 성공");
+		logger.debug("MainController.goMain : 데이터 조회");
 
 		String userid ;
 
@@ -24,14 +36,13 @@ public class MainController {
 			userid = mainService.selectOneUserId();
 
 			if ( !"".equals(userid) && userid != null ) {
-				System.out.println("############MainController.goMain : 데이터 정상 조회 : " + userid);
+				logger.debug("MainController.goMain : 데이터 조회 성공, id = " + userid);
 			} else {
-				System.out.println("############MainController.goMain : 데이터 조회 실패 : 공백 or null");
+				logger.debug("MainController.goMain : 데이터 조회 실패");
 			}
-
 		} catch(Exception e) {
-			System.out.println("############MainController.goMain : 데이터 조회 중 에러 발생");
-			System.out.println(e.getMessage());
+			logger.error("MainController.goMain : 데이터 조회 중 에러 발생");
+			logger.error("[Exception] MainController.goMain : " + e.getMessage());
 		}
 		return "main/main";
 	}
