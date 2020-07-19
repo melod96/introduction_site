e62c0aff87394e71c97e4978237a542a06d02341
hw.lee <hyeongu0305@gmail.com>
2020-07-19 20:12:18 +0900

    주석 추가

--- introduction_site/src/main/java/com/introsite/Application.java
+++ introduction_site/src/main/java/com/introsite/Application.java
@@ -18,11 +18,17 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
+    @Bean
+    // sqlSession을 생성할 sqlSessionFactory를 생성 및 Bean 등록
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        /* DataSource 
+        : Connection Pool을 구현하기 위한 스펙을 정해놓은 인터페이스.
+        DB와의 연동을 위해 사용되는 객체이며, application.properties에 connection 관련 내용을 작성해두었다.
+        */
         sessionFactory.setDataSource(dataSource);
 
+        // 실제 쿼리가 들어갈 xml 패키지 경로를 입력
         Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
         sessionFactory.setMapperLocations(res);
 
